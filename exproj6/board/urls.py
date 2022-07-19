9df3ab5fc0b3934612647063b5cdbf833f45065d
June <6ixthmonth@gmail.com>
2022-07-19 17:06:11 +0900

    Edit tests

--- exproj6/board/urls.py
+++ exproj6/board/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 app_name = 'board'
 urlpatterns = [
-    path('list/', views.ListView.as_view(), name='list'),  # 게시글 목록
-    path('<int:board_number>/', views.DetailView.as_view(), name='detail') # 게시글 상세
+    path('', views.ListView.as_view(), name='list'),  # 게시글 목록.
+    path('<int:board_number>/', views.DetailView.as_view(), name='detail') # 게시글 상세.
 ]
--- exproj6/user/urls.py
+++ exproj6/user/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 app_name = 'user'
 urlpatterns = [
-    path('signup/', views.create, name='create'),  # 사용자 등록
-    path('signin/', views.form, name='form'),  # 로그인
-    path('signout/', views.signout, name='signout'),  # 로그아웃
+    path('join/', views.user_create, name='create'),  # 사용자 등록.
+    path('login/', views.user_login, name='login'),  # 로그인.
+    path('logout/', views.user_logout, name='logout'),  # 로그아웃.
 ]
