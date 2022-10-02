c2254863f1128ac427a189c4ad87cb93da2d26c1
June <42332051+6ixthmonth@users.noreply.github.com>
2022-10-02 14:23:57 +0900

    Edit document

--- myboard/user/urls.py
+++ myboard/user/urls.py
@@ -7,12 +7,12 @@ from . import views
 app_name = 'user'
 urlpatterns = [
 
-    # 로그인 뷰를 연결하는 URL 패턴. 빌트-인 뷰를 사용하여 로그인 페이지로 이동하거나 로그인을 처리한다. 다음과 같은 인수를 전달할 수 있다.
+    # 로그인 뷰를 연결하는 URL 패턴. 내장 뷰를 사용하여 로그인 페이지로 이동하거나 로그인을 처리한다. 다음과 같은 인수를 전달할 수 있다.
     # template_name: 로그인 하고자 할 때(GET 요청 시) 표시할 템플릿 이름. 즉, 로그인 페이지에 해당하는 HTML 파일의 경로와 이름. 기본 값은 'registration/login.html'.
     # next_page: 로그인 처리 후(POST 요청 시) 리다이렉트할 URL. 기본 값은 '/accounts/profile/'.
     path('login/', auth_views.LoginView.as_view(template_name='user/login.html', next_page='/'), name='login'),
 
-    # 로그아웃 뷰를 연결하는 URL 패턴. 빌트-인 뷰를 사용하여 로그아웃을 처리한다. 다음과 같은 인수를 전달할 수 있다.
+    # 로그아웃 뷰를 연결하는 URL 패턴. 내장 뷰를 사용하여 로그아웃을 처리한다. 다음과 같은 인수를 전달할 수 있다.
     # template_name: 로그아웃 처리 후 표시할 템플릿 이름. 리다이렉트 하지 않음. 기본 값은 'registration/logged_out.html'.
     # next_page: 로그아웃 처리 후 리다이렉트할 URL. 기본 값은 None.
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
--- myboard/user/views.py
+++ myboard/user/views.py
@@ -3,16 +3,20 @@ from django.views.generic import CreateView, UpdateView, DeleteView
 
 
 class UserCreateView(CreateView):
-    """사용자 등록 뷰. 빌트-인 뷰인 CreateView를 상속하여 구현한 사용자 정의 뷰로써, 사용자 등록 페이지로 이동하거나(GET 요청 시), 신규 사용자 데이터를 저장한다(POST 요청 시)."""
+    """
+    사용자 등록 뷰.
+    내장 뷰 클래스인 CreateView를 상속하여 구현한 사용자 정의 뷰.
+    GET 요청 시, 사용자 등록 페이지로 이동하며, POST 요청 시, 전달받은 신규 사용자 데이터를 저장한다.
+    """
 
-    # CreateView는 모델과 필드를 각각 따로 등록하거나, 폼을 이용해서 한꺼번에 등록하여 사용하는 방법이 있다.
+    # CreateView는 모델과 필드를 각각 따로 등록하거나, 폼을 이용해서 한꺼번에 등록해서 사용한다.
 
     # 1. 이 뷰에서 다룰 모델 클래스와, 웹 페이지에 표시할 필드 목록을 각각 따로 정의하는 방법.
     # from django.contrib.auth.models import User
     # model = User  # 이 뷰에서 다룰 모델 클래스. 여기서는 빌트-인 사용자 클래스인 User 클래스를 적용한다.
     # fields = ['username', 'password']  # 웹 페이지에서 입력받기 위해 표시할 필드 목록. User 클래스가 가진 필드 중 표시하기 원하는 걸 골라서 작성한다.
 
-    # 1-2. 기존 모델 클래스의 필드 외 표시하고 싶은 필드가 있으면 사용자 정의 모델 클래스를 사용한다.
+    # 1-2. 빌트인 모델 클래스의 필드 외 표시하고 싶은 필드가 있으면 사용자 정의 모델 클래스를 사용한다.
     # from .models import CustomUser
     # model = CustomUser  # 빌트-인 모델 클래스인 User 클래스를 상속하여 구현한 사용자 정의 모델 클래스.
     # fields = ['username', 'password', 'password2']
