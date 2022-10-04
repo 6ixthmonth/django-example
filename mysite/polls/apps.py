50482d4430b5e39c439bc886367e3447f724023d
June <42332051+6ixthmonth@users.noreply.github.com>
2022-10-04 16:49:49 +0900

    Edit document

--- mysite/polls/apps.py
+++ mysite/polls/apps.py
@@ -2,7 +2,7 @@ from django.apps import AppConfig
 
 
 class PollsConfig(AppConfig):
-    """polls 앱을 설정 파일에 등록할 때 사용하는 클래스."""
+    """polls 앱을 설정 파일에 등록할 때 사용하는 앱 구성 클래스."""
 
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'polls'
