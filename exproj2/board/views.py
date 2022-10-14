91ff37b160b4a501b8aa3ed812d44e1d3af18efb
June <42332051+6ixthmonth@users.noreply.github.com>
2022-10-14 15:21:02 +0900

    Refine example code and edit documents

--- exproj2/board/views.py
+++ exproj2/board/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 
 
 def board_list(request):
-    """게시글 목록 뷰 함수."""
-    return HttpResponse("게시글 목록")
+    """게시판 뷰 함수."""
+    return HttpResponse("게시판")
--- exproj2/exproj2/urls.py
+++ exproj2/exproj2/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', include('user.urls')),  # 사용자 관련 앱 (사용자 등록, 로그인 등)
-    path('board/', include('board.urls')),  # 게시판 관련 앱 (게시글 목록 등)
+    path('user/', include('user.urls')),  # 사용자 앱 URL 패턴.
+    path('board/', include('board.urls')),  # 게시판 앱 URL 패턴.
 ]
