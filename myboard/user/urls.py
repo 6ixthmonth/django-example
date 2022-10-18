632910fdd36c586db84b0db017579cb2f5368a93
June <42332051+6ixthmonth@users.noreply.github.com>
2022-10-18 16:48:47 +0900

    Edit examples

--- myboard/user/urls.py
+++ myboard/user/urls.py
@@ -1,5 +1,5 @@
-from django.urls import path
-from django.contrib.auth import views as auth_views
+from django.urls import path, reverse_lazy
+from django.contrib.auth.views import LoginView, LogoutView
 
 from . import views
 
@@ -8,16 +8,23 @@ app_name = 'user'
 urlpatterns = [
 
     # 로그인 뷰를 연결하는 URL 패턴. 내장 뷰를 사용하여 로그인 페이지로 이동하거나 로그인을 처리한다. 다음과 같은 인수를 전달할 수 있다.
-    # template_name: 로그인 하고자 할 때(GET 요청 시) 표시할 템플릿 이름. 즉, 로그인 페이지에 해당하는 HTML 파일의 경로와 이름. 기본 값은 'registration/login.html'.
-    # next_page: 로그인 처리 후(POST 요청 시) 리다이렉트할 URL. 기본 값은 '/accounts/profile/'.
-    path('login/', auth_views.LoginView.as_view(template_name='user/login.html', next_page='/'), name='login'),
+    # # template_name
+    # # # GET 요청을 처리할 때 응답할 템플릿 파일 이름. 기본 값은 'registration/login.html'.
+    # # # 로그인 페이지 템플릿 파일 이름을 설정한다.
+    # # next_page
+    # # # POST 요청을 처리한 후 리다이렉트 할 URL 문자열 값. 기본 값은 '/accounts/profile/'.
+    # # # 요청에 next 인수가 있으면 next 인수 값으로 리다이렉트 한다.
+    path('login/', LoginView.as_view(template_name='user/login.html', next_page=reverse_lazy('home')), name='login'),
 
-    # 로그아웃 뷰를 연결하는 URL 패턴. 내장 뷰를 사용하여 로그아웃을 처리한다. 다음과 같은 인수를 전달할 수 있다.
-    # template_name: 로그아웃 처리 후 표시할 템플릿 이름. 리다이렉트 하지 않음. 기본 값은 'registration/logged_out.html'.
-    # next_page: 로그아웃 처리 후 리다이렉트할 URL. 기본 값은 None.
-    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    # 로그아웃 뷰를 연결하는 URL 패턴. 내장 뷰를 사용하여 로그아웃을 처리한다. 다음의 인수들 중 하나를 설정할 수 있다.
+    # # template_name
+    # # # 로그아웃 처리 후 응답할 템플릿 파일 이름. 기본 값은 'registration/logged_out.html'.
+    # # # 장고 설치 시 기본적으로 제공되는 템플릿 파일이기 때문에 이 인수를 생략해도 정상적으로 동작한다.
+    # # next_page
+    # # # 로그아웃 처리 후 리다이렉트할 URL. 문자열 값. 기본 값은 None.
+    path('logout/', LogoutView.as_view(next_page=reverse_lazy('home')), name='logout'),
 
-    # 사용자 등록 뷰를 연결하는 URL 패턴. 사용자 정의 뷰를 사용하여 사용자 등록 페이지로 이동하거나 신규 사용자 데이터를 저장한다.
+    # 사용자 등록 뷰를 연결하는 URL 패턴.
     path('join/', views.UserCreateView.as_view(), name='create'),
 
     # 사용자 정보 수정 뷰를 연결하는 URL 패턴.
