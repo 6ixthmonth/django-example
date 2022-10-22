46982c233e88a816055ce320aedd11e38b3378b2
June <42332051+6ixthmonth@users.noreply.github.com>
2022-10-22 16:16:11 +0900

    Edit documents

--- exproj3/user/urls.py
+++ exproj3/user/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path('join/', views.user_create),  # 사용자 등록.
-    path('login/', views.user_login),  # 로그인.
+    path('join/', views.user_create),  # 사용자 등록 URL 패턴.
+    path('login/', views.user_login),  # 로그인 URL 패턴.
 ]
