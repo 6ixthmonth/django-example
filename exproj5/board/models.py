5715ab9210c572d0d85c03d140b7361be0acd35b
June <42332051+6ixthmonth@users.noreply.github.com>
2022-11-01 16:38:31 +0900

    Edit examples

--- exproj5/board/models.py
+++ exproj5/board/models.py
@@ -7,13 +7,13 @@ from user.models import User
 
 
 class Board(models.Model):
-    """게시글 모델을 표현하는 클래스."""
+    """게시글을 표현하는 모델 클래스."""
 
-    number = models.BigAutoField(primary_key=True)  # 게시글 번호.
-    title = models.CharField(max_length=40)  # 게시글 제목.
-    content = models.TextField(null=True, blank=True)  # 게시글 내용.
-    date = models.DateTimeField(default=timezone.now)  # 게시글 작성일.
-    user = models.ForeignKey(User, on_delete=models.CASCADE)  # 게시글 작성자.
+    number = models.BigAutoField(primary_key=True)  # 번호.
+    title = models.CharField(max_length=40)  # 제목.
+    content = models.TextField(null=True, blank=True)  # 내용.
+    date = models.DateTimeField(default=timezone.now)  # 작성일.
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)  # 작성자.
 
     # 관리자 페이지에서 Board 객체 출력 시 사용.
     def __str__(self) -> str:
@@ -21,32 +21,30 @@ class Board(models.Model):
 
     # 파이썬 셸에서 Board 객체 출력 시 사용.
     def __repr__(self) -> str:
-        # 가장 긴 이름을 가진 속성의 글자 수를 구한다.
-        max_attr_name_length = 0
+        attr_name_max_length = 0
         for attr_name in vars(self).keys():
-            max_attr_name_length = len(attr_name) if len(attr_name) > max_attr_name_length and not attr_name.startswith("_") else max_attr_name_length
-
+            attr_name_max_length = len(attr_name) if not attr_name.startswith("_") and len(attr_name) > attr_name_max_length else attr_name_max_length
         return f"""{{
-            {'number':{max_attr_name_length}}: {self.number},
-            {'title':{max_attr_name_length}}: "{shorten(self.title, width=20, placeholder='...')}",
-            {'content':{max_attr_name_length}}: "{shorten(self.content, width=20, placeholder='...') if self.content != None else ''}",
-            {'date':{max_attr_name_length}}: {self.date:%Y-%m-%d %H:%M:%S},
-            {'user_id':{max_attr_name_length}}: {self.user_id},
+            {'number':{attr_name_max_length}}: {self.number},
+            {'title':{attr_name_max_length}}: "{shorten(self.title, width=20, placeholder='...')}",
+            {'content':{attr_name_max_length}}: "{shorten(self.content, width=20, placeholder='...') if self.content != None else ''}",
+            {'date':{attr_name_max_length}}: {self.date:%Y-%m-%d %H:%M:%S},
+            {'user_id':{attr_name_max_length}}: {self.user_id},
         }}"""
 
     def is_today_published(self) -> bool:
-        """오늘 작성한 게시글 여부를 반환하는 함수."""
+        """오늘 작성 여부를 반환하는 함수."""
         board_date = self.date  # 게시글 작성일.
-        today = timezone.now()  # 현재 시각.
+        today = timezone.now()  # 현재 시간.
         return (board_date.year == today.year) and (board_date.month == today.month) and (board_date.day == today.day)
 
 
 class Reply(models.Model):
-    """댓글 모델을 표현하는 클래스."""
+    """댓글을 표현하는 모델 클래스."""
 
-    content = models.TextField()  # 댓글 내용.
-    date = models.DateTimeField(default=timezone.now)  # 댓글 작성일.
-    user = models.ForeignKey(User, on_delete=models.CASCADE)  # 댓글 작성자.
+    content = models.TextField()  # 내용.
+    date = models.DateTimeField(default=timezone.now)  # 작성일.
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)  # 작성자.
     board = models.ForeignKey(Board, on_delete=models.CASCADE)  # 원본 게시글.
 
     # 관리자 페이지에서 Reply 객체 출력 시 사용.
@@ -55,15 +53,13 @@ class Reply(models.Model):
 
     # 파이썬 셸에서 Reply 객체 출력 시 사용.
     def __repr__(self) -> str:
-        # 가장 긴 이름을 가진 속성의 글자 수를 구한다.
-        max_attr_name_length = 0
+        attr_name_max_length = 0
         for attr_name in vars(self).keys():
-            max_attr_name_length = len(attr_name) if len(attr_name) > max_attr_name_length and not attr_name.startswith("_") else max_attr_name_length
-
+            attr_name_max_length = len(attr_name) if not attr_name.startswith("_") and len(attr_name) > attr_name_max_length else attr_name_max_length
         return f"""{{
-            {'id':{max_attr_name_length}}: {self.id},
-            {'content':{max_attr_name_length}}: "{shorten(self.content, width=20, placeholder='...')}",
-            {'date':{max_attr_name_length}}: {self.date:%Y-%m-%d %H:%M:%S},
-            {'user_id':{max_attr_name_length}}: {self.user_id},
-            {'board_id':{max_attr_name_length}}: {self.board_id}
+            {'id':{attr_name_max_length}}: {self.id},
+            {'content':{attr_name_max_length}}: "{shorten(self.content, width=20, placeholder='...')}",
+            {'date':{attr_name_max_length}}: {self.date:%Y-%m-%d %H:%M:%S},
+            {'user_id':{attr_name_max_length}}: {self.user_id},
+            {'board_id':{attr_name_max_length}}: {self.board_id}
         }}"""
--- exproj5/board/urls.py
+++ exproj5/board/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 app_name = 'board'
 urlpatterns = [
-    path('', views.board_list, name='list'),  # 게시글 목록.
-    path('<int:board_number>/', views.board_detail, name='detail') # 게시글 상세.
+    path('', views.board_list, name='list'),  # 게시판 URL 패턴.
+    path('<int:board_number>/', views.board_detail, name='detail'),  # 게시글 상세 URL 패턴.
 ]
