df514da1bb03b54c7bf259b09cfb30e71a5ec020
June <42332051+6ixthmonth@users.noreply.github.com>
2022-11-02 14:57:03 +0900

    Edit examples

--- exproj5/board/views.py
+++ exproj5/board/views.py
@@ -5,7 +5,6 @@ from .models import Board
 
 def board_list(request):
     """게시글 목록 뷰 함수."""
-    # 데이터베이스에서 모든 게시글 데이터를 날짜 역순으로 가져와서 웹 페이지로 전달한다.
     board_list = Board.objects.order_by('-date')
     context = {
         'board_list': board_list,
@@ -15,8 +14,7 @@ def board_list(request):
 
 def board_detail(request, board_number):
     """게시글 상세 뷰 함수."""
-    # 데이터베이스에서 특정 게시글 데이터를 가져와서 웹 페이지로 전달한다.
-    board = get_object_or_404(Board, pk=board_number)  # 전달받은 게시글 번호에 해당하는 데이터가 없으면 404 오류를 발생시킨다.
+    board = get_object_or_404(Board, pk=board_number)
     context = {
         'board': board,
     }
--- exproj5/exproj5/urls.py
+++ exproj5/exproj5/urls.py
@@ -7,6 +7,6 @@ from user import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
-    path('user/', include('user.urls')),  # 사용자 관련 앱 (사용자 등록, 로그인 등)
-    path('board/', include('board.urls')),  # 게시판 관련 앱 (게시글 목록 등)
+    path('user/', include('user.urls')),  # 사용자 앱 URL 패턴.
+    path('board/', include('board.urls')),  # 게시판 앱 URL 패턴.
 ]
