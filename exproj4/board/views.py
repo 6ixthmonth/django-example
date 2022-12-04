be5203055e851941eefd9135da5cde15c579ea03
June <42332051+6ixthmonth@users.noreply.github.com>
2022-12-04 14:17:11 +0900

    Delete unnecessary files

--- exproj4/board/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-
-
-def board_list(request):
-    """게시판 뷰 함수."""
-    return HttpResponse("게시판")
--- exproj4/user/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-
-urlpatterns = [
-    path('join/', views.user_create),
-    path('login/', views.user_login),
-]
