30abec1cd19b4e10c4984fafa5180c7a3b6c4301
June <42332051+6ixthmonth@users.noreply.github.com>
2022-12-09 13:51:16 +0900

    Edit examples

--- myexercise5/board/urls.py
+++ myexercise5/board/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 
+app_name = 'board'
 urlpatterns = [
-    path('', views.board_list),
+    path('', views.board_list, name='list'),
+    path('<int:board_number>/', views.board_detail, name='detail'),
 ]
--- myexercise5/user/urls.py
+++ myexercise5/user/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 
+app_name = 'user'
 urlpatterns = [
-    path('join/', views.user_create),
-    path('login/', views.user_login),
+    path('join/', views.user_create, name='create'),
+    path('login/', views.user_login, name='login'),
 ]
