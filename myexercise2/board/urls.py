5e72472037be631c3c6320caa189f4cbd9fd75e7
June <42332051+6ixthmonth@users.noreply.github.com>
2022-12-15 16:30:04 +0900

    Edit documents

--- myexercise2/board/urls.py
+++ myexercise2/board/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.board_list),
+    path('<int:board_number>', views.board_detail),
 ]
--- myexercise2/board/views.py
+++ myexercise2/board/views.py
@@ -2,5 +2,10 @@ from django.http import HttpResponse
 
 
 def board_list(request):
-    """게시판 뷰 함수."""
+    """게시판 뷰."""
     return HttpResponse("게시판")
+
+def board_detail(request, board_number):
+    """게시글 상세 뷰."""
+    print("조회하고자 하는 게시글 번호: %d" % board_number)
+    return HttpResponse("%d번 게시글 상세" % board_number)
