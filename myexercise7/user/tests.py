ec7696ca952163b54b8d87204102c058bce127da
June <42332051+6ixthmonth@users.noreply.github.com>
2022-12-26 13:37:24 +0900

    Delete unnecessary files

--- myexercise7/user/tests.py
+++ myexercise7/user/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import User
+
+
+def create_user(id="tester", pw="1234", nm="검사자") -> User:
+    """검사에 사용할 사용자를 생성하는 함수."""
+    return User.objects.create(id=id, pw=pw, nm=nm)
